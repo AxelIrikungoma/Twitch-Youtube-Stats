ecf6e38896346b1fd2c08123c6387bacc2d8e4ab
Axel Irikungoma <axel.irikungoma@vanderbilt.edu>
2021-07-27 18:38:16 +0000

    testing CD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,8 @@
-# import sys
+import sys
 
-# sys.path.append('../Twitch-Youtube-Stats')
-# from Web import app  # imports flask app object
+sys.path.append('../Twitch-Youtube-Stats')
+from Web import app  # imports flask app object
 
-# def test_index(app, client):
-#     res = client.get('/')
-#     assert res.status_code == 200
+def test_index(app, client):
+    res = client.get('/')
+    assert res.status_code == 200
